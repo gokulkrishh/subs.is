d09131a5b3e7728cbf2733b94c89d356dbea9123
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-03-30 13:20:41 +0530

    update theme color for dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export const viewport: Viewport = {
   viewportFit: 'cover',
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: '#ffffff' },
-    { media: '(prefers-color-scheme: dark)', color: '#000000' },
+    { media: '(prefers-color-scheme: dark)', color: '#0a0a0a' },
   ],
 };
 
