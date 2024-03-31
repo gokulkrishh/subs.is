8b0560e0bae82bc5e53bdd5f036fc563109e8cf3
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-03-31 09:43:20 +0530

    fix squential promise calls for page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import Card from 'components/card';
-import Summary from 'components/summary';
-import { navFilter, summaryFilter } from 'config/data';
 
 import { getSubscriptions } from './actions/subscriptions';
 import { getUser } from './actions/user';
 
 export default async function Page() {
-  const user = await getUser();
-  const subscriptions = await getSubscriptions();
+  const [user, subscriptions] = await Promise.all([await getUser(), await getSubscriptions()]);
 
   return (
     <main className="flex flex-col mt-10">
