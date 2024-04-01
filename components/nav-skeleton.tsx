1d1a984972bd3b20877b8a11865c75b4c724c095
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-04-01 09:38:33 +0530

    fix loader

--- components/nav-skeleton.tsx
+++ components/nav-skeleton.tsx
@@ -2,9 +2,9 @@ import { Skeleton } from './ui/skeleton';
 
 export default function NavSkeleton() {
   return (
-    <div className="flex justify-between items-center">
-      <h2 className="md:text-lg font-semibold">Subscriptions</h2>
-      <Skeleton className="flex h-[38px] w-[181px] border border-input rounded-md" />
+    <div className="flex justify-between sm:items-center flex-col gap-4 sm:flex-row">
+      <h2 className="text-lg font-semibold">Subscriptions</h2>
+      <Skeleton className="flex max-sm:self-end h-[42px] w-[250px] border border-input rounded-md" />
     </div>
   );
 }
