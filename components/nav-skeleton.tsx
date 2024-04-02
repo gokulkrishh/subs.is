446e62b5cf0978242612db63bfaa4e5a09fd5c7c
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-04-02 18:53:04 +0530

    fix share + nav skeleton

--- components/nav-skeleton.tsx
+++ components/nav-skeleton.tsx
@@ -2,9 +2,9 @@ import { Skeleton } from './ui/skeleton';
 
 export default function NavSkeleton() {
   return (
-    <div className="flex justify-between sm:items-center flex-col gap-4 sm:flex-row">
+    <div className="flex justify-between h-[42px] sm:h-[38px] sm:items-center flex-col gap-4 sm:flex-row">
       <h2 className="text-lg font-semibold">Subscriptions</h2>
-      <Skeleton className="flex max-sm:self-end h-[42px] w-[250px] border border-input rounded-md" />
+      <Skeleton className="flex max-sm:self-end h-[42px] w-[286px] border border-input rounded-md" />
     </div>
   );
 }
