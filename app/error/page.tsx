17b86815b458564f29e373dbe117854b1b4cd53c
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-04-15 22:21:13 +0530

    update the error page

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -5,14 +5,13 @@ import { socialUrls } from 'config/urls';
 export default async function Page() {
   return (
     <div className="flex flex-col justify-center mt-28 gap-2">
-      <h2 className="font-medium text-lg">Thank you for requesting early access.</h2>
+      <h2 className="font-medium text-lg">Error Occurred</h2>
       <p className="text-muted-foreground">
-        We are currently in private beta, dm me in{' '}
-        <Link className="border-b hover:border-primary hover:text-primary" target="_blank" href={socialUrls.twitter}>
+        An error occurred during the sign in, drop an email to
+        <Link className="border-b hover:border-primary hover:text-primary" target="_blank" href={socialUrls.help}>
           {' '}
-          twitter
+          support@subs.is
         </Link>{' '}
-        to get access.
       </p>
     </div>
   );
