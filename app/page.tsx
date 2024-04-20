49d9b04b957967ade20b81d8a7f912b3770fc67d
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-04-20 08:25:02 +0530

    fix loader

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import { getUser } from './actions/user';
 
 export default async function Page() {
   const [user, subscriptions] = await Promise.all([await getUser(), await getSubscriptions()]);
+  // sleep 10secs
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   return (
     <main className="flex flex-col mt-10">
