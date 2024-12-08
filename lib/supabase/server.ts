1c35867bb4cf11a89663eb56c201c364a7d6fe81
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-12-08 20:36:13 +0530

    upgrade pkgs

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -2,7 +2,7 @@ import { cookies } from 'next/headers';
 
 import { createServerClient } from '@supabase/ssr';
 
-export const createClient = async (cacheTags: string[] = ['supabase'], forceCache: boolean = false) => {
+export const createClient = async () => {
   const cookieStore = await cookies();
   return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
     cookies: {
