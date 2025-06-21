7db013177886cbc73157437255711883c37a8326
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2025-06-21 18:30:53 +0530

    initialize Next.js project with essential configurations and dependencies

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="grid items-center justify-items-center min-h-screen">
+      <main>
+        <h1>Hello World</h1>
+      </main>
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </div>
+  );
+}
