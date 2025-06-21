83737e2902ae7334e17ade87244a2d9b4275694f
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2025-06-21 18:54:40 +0530

    Refactor layout and page components to improve metadata and clean up structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 export default function Home() {
   return (
     <div className="grid items-center justify-items-center min-h-screen">
-      <main>
-        <h1>Hello World</h1>
-      </main>
-      <footer>
-        <p>Footer</p>
-      </footer>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
